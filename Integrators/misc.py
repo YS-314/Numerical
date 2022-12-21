726a8c9643fddee7386281d709ba0558160ba66c
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-21 21:11:00 +0800

    Need to assign each index of X as coeff

--- Integrators/misc.py
+++ Integrators/misc.py
@@ -3,9 +3,9 @@ import numpy as np
 def tri2darr(*coeff):
   n = int(1/2*((8*len(coeff)+1)**(1/2)-1))
   j = int(1)
-  X = []
+  X = np.zeros((n,n))
   cl = int(0)
-  while j < n:
+  #while j < n:
     cl2 = cl + j
     #0.5*j*(j+1)
     vec = np.concatenate(coeff[cl:cl2],np.zeros(n-cl))
