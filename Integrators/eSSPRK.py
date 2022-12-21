85195949b79e9b78c34ad9566673459898e9b2f3
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-21 12:25:30 +0800

    Update eSSPRK.py

--- Integrators/eSSPRK.py
+++ Integrators/eSSPRK.py
@@ -1,4 +1,4 @@
 #explicit strong stability preserving Runge-Kutta methods (s,n) of stages s and order n
-#integrates semi-discretised hyperbolic conservation laws of d/dt U = F(U)
+#integrates semi-discretised hyperbolic conservation laws of d/Udt = F(U)
 
 #Shu-Osher (3,3)
