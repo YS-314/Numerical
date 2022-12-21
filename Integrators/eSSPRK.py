eb251226ec404691168f5ea77e39fcb4e36bb8f2
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-21 18:25:21 +0800

    Delete eSSPRK.py

--- Integrators/eSSPRK.py
+++
@@ -1,9 +0,0 @@
-#explicit strong stability preserving Runge-Kutta methods (s,n) of stages s and order n
-#integrates semi-discretised hyperbolic conservation laws of d/Udt = F(U)
-import numpy as np
-import math
-
-#Shu-Osher (3,3)
-def eSSP33(ab):
-    if ab == 1:
-        x = np.array([])
