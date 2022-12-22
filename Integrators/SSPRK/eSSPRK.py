cc15cc0d47eb929640f9c0396b5e3c6b95268414
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-22 20:05:29 +0800

    support for adaptive algorithms

--- Integrators/SSPRK/eSSPRK.py
+++ Integrators/SSPRK/eSSPRK.py
@@ -8,20 +8,29 @@ import math
 class eSSP33(eSSPRK):
     alpha = tri2darr(1,3/4,1/4,1/3,0,2/3)
     beta = tri2darr(1,0,1,0,0,1)
-
+    s = 3
+    p = 3
+    adaptive = False
+#eSSPRK(2,2)
 class eSSP22(eSSPRK):
     alpha = tri2darr(1,1/2,1/2)
     beta = tri2darr(1,0,1)
-
+    s = 2
+    p = 2
+    adaptive = False
+#eSSPRK(5,4)
 class eSSP54(eSSPRK):
     alpha = tri2darr(1,0.444370493651235,0.555629506348765,0.620101851488403,0,0.379898148511597,0.178079954393132,0,0,0.821920045606868,0,0,0.517231671970585, 0.096059710526147,0.386708617503268 )
     beta = tri2darr(0.391752226571890,0, 0.368410593050371,0,0,0.251891774271694,0,0,0,0.544974750228521,0,0,0,0.063692468666290,0.226007483236906) 
+    s = 5
+    p = 4
+    adaptive = False
 
 es_METHODS = {'eSR33': eSSP33,
            'eSR22': eSSP22,
            'eSR54': eSSP54}
 
-def eSSP_int(func,t,U,tn,h,method='eSR33',kl):
+def eSSP_int(func,t,U,tn,h,method='eSR33',kl=False):
     """
     1 step of eSSP integration
     func: callable function
@@ -38,7 +47,8 @@ def eSSP_int(func,t,U,tn,h,method='eSR33',kl):
         method = es_METHODS[method]
     alpha = method.alpha
     beta = method.beta
-    
-    for i in t:
-        Ua
+    s = method.s
+    adaptive = method.adaptive
+    if adaptive = false:
+        
 
