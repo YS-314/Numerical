3a3cdbe9b09d4a0775f45c629547753cfaf48288
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-22 12:04:26 +0800

    tri2darr

--- Integrators/misc.py
+++ Integrators/misc.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-def tri2darr(*coeff):
+def tri2darr(coeff):
   n = int(1/2*((8*len(coeff)+1)**(1/2)-1))
-  j = int(1)
-  X = np.zeros((n,n))
-  cl = int(0)
-  #while j < n:
-    cl2 = cl + j
-    #0.5*j*(j+1)
-    vec = np.concatenate(coeff[cl:cl2],np.zeros(n-cl))
-    #np.full((n-cl),0)?
-    cl = cl2
-    X = np.vstack(X,vec)
+  j = 0
+  X = np.empty((n,n))
+  cl = 0
+  while j < n:
+    for x in range(0,j+1):
+      X[j,x] = coeff[cl+x]
+    j += 1
+    cl += j
+  return X
