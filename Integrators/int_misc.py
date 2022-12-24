6a65d47fe8039c2139637a6c853cb25bb126099b
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-24 12:26:39 +0800

    Update and rename misc.py to int_misc.py

--- Integrators/int_misc.py
+++ Integrators/int_misc.py
@@ -3,7 +3,7 @@ import numpy as np
 def tri2darr(*coeff):
   n = int(1/2*((8*len(coeff)+1)**(1/2)-1))
   j = 0
-  X = np.empty((n,n))
+  X = np.zeros((n,n))
   cl = 0
   while j < n:
     for x in range(0,j+1):
