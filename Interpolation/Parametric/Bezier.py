a0f30718de303656cdd8ff975d95ab6a7805e8d9
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-27 12:53:24 +0800

    Update Bezier.py

--- Interpolation/Parametric/Bezier.py
+++ Interpolation/Parametric/Bezier.py
@@ -1,5 +1,10 @@
 import numpy as np
 import math
 def Bernstein(i,n,u):
-  math.factorial(
-def Bezier(P,
+  return math.comb(n,i)*math.pow(u,i)*math.pow(1-u,n-i)
+
+def BP_C(t,deg,dim):
+  for n in range()
+
+def Bezier(P,t,deg,dim): # dim = P.ndim, which is length of t (a vector)
+  BPcf = 
