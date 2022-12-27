de061c3adb2d69bbb596f0c0d4739b25a6ce4c53
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-27 20:28:08 +0800

    Roe&Rusanov flux

--- PDE solvers/H.C.L/flux_splitting.py
+++ PDE solvers/H.C.L/flux_splitting.py
@@ -1,1 +1,22 @@
+import numpy as np
+import math
+
+'''
+n: (integer) index of split flux
+pm: (±1) positive or negative flux
+fi: (callable) f(u(xi))
+'''
+
+#Roe flux
+#fp: (callable) derivative of f
+def roe(n,pm,fi,fp):
+    
+
+
+#Local Lax-Friedrichs / Rusanov
+#dfu: (np array)stencil of derivative of f(u) at fi
+def LFs(n,pm,dfu,fi):
+    alpha = np.max(dfu)
+    return 1/2*(fi(n)+pm*alpha)
+
 
