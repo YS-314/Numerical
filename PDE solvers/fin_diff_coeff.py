0cf3cf243147592e1c7526c9311a2f7f0a9e8ea1
YS-314 <120360443+YS-314@users.noreply.github.com>
2022-12-30 20:47:02 +0800

    Update fin_diff_coeff.py

--- PDE solvers/fin_diff_coeff.py
+++ PDE solvers/fin_diff_coeff.py
@@ -2,6 +2,8 @@ import numpy as np
 import math
 from scipy import linalg
 
+#credits: https://web.media.mit.edu/~crtaylor/calculator.html and https://en.wikipedia.org/wiki/Finite_difference_coefficient
+
 def kron_delta(i,j):
     if i == j:
         return 1
