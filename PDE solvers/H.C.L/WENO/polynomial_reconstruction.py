391e131a318b9c1e7a8097003010e7b7c693f6a1
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-02 20:03:24 +0800

    easy sum

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -14,3 +14,11 @@ def sum_neq(m0,n,neq,f):
 
 def crj_u(r,s,j):
     k = r+s+1
+
+def sum_neq_ez(m0,n,neq,f): #computes unnecessary terms
+    X = 0
+    for m in range(m0, n+1):
+        X += f(m)
+    for m in range(0,len(neq)):
+        X -= f(neq[m])
+    return X
