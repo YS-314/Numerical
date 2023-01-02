75c6879a33236ec8589f83fdb3dd967e87a9e094
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-02 19:59:44 +0800

    fixed not equal sums

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -1,2 +1,16 @@
 import math
 import numpy
+
+def sum_neq(m0,n,neq,f):
+    X = 0
+    for m in range(m0,neq[0]):
+        X += f(m)
+    for M in range1(len(neq),1): 
+        for m in range1(neq[M],neq[M-1]+1):
+            X += f(m)
+    for m in range(neq[len(neq)-1]+1,n+1): #correct
+        X += f(m)
+    return X
+
+def crj_u(r,s,j):
+    k = r+s+1
