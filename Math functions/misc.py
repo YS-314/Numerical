b0c5a7d7b55ad0a23d23d95723b571fd8f3f5335
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 11:16:47 +0800

    Update misc.py

--- Math functions/misc.py
+++ Math functions/misc.py
@@ -1,1 +1,31 @@
 
+#Sum of f(x) for x from m0 to n excluding neq
+'''
+m0: start of sum (int)
+n: end of sum (int)
+neq: values to skip (1D list of int)
+f: function to rake sum of (callable)
+'''
+def sum_neq(m0,n,neq,f):
+    X = 0
+    lenneq = len(neq)
+    for m in range(m0,neq[0]):
+        X += f(m)
+    if lenneq > 1:
+        for M in range1(lenneq,1):
+            if neq[M]-neq[M-1] > 1:
+                for m in range1(neq[M],neq[M-1]+1):
+                    X += f(m)
+    for m in range(neq[lenneq-1]+1,n+1):
+        X += f(m)
+    return X
+  
+#inputs same as sum_neq
+#more naive implementation: computes unnecessary (skipped) terms. Not suitable if function is undefined at the points.
+def sum_neq_ez(m0,n,neq,f):
+    X = 0
+    for m in range(m0, n+1):
+        X += f(m)
+    for m in range(0,len(neq)):
+        X -= f(neq[m])
+    return X
