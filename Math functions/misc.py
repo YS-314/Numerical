d826f8a2da444f5cbb90dbf27ca530284c23ceba
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 11:44:38 +0800

    Update misc.py

--- Math functions/misc.py
+++ Math functions/misc.py
@@ -19,13 +19,36 @@ def sum_neq(m0,n,neq,f):
     for m in range(neq[lenneq-1]+1,n+1):
         X += f(m)
     return X
-  
-#inputs same as sum_neq
+
+def prod_neq(m0,n,neq,f):
+    X = 1
+    lenneq = len(neq)
+    for m in range(m0,neq[0]):
+        X *= f(m)
+    if lenneq > 1:
+        for M in range1(lenneq,1):
+            if neq[M]-neq[M-1] > 1:
+                for m in range1(neq[M],neq[M-1]+1):
+                    X *= f(m)
+    for m in range(neq[lenneq-1]+1,n+1):
+        X *= f(m)
+    return X
+
+#inputs same as sum_neq / prod_neq
 #more naive implementation: computes unnecessary (skipped) terms. Not suitable if function is undefined at the points.
+#Accuracy might be slightly lower than functions above due to floating point error caused by multiplying then dividing skipped terms
 def sum_neq_ez(m0,n,neq,f):
     X = 0
     for m in range(m0, n+1):
         X += f(m)
     for m in range(0,len(neq)):
         X -= f(neq[m])
+    return X
+
+def prod_neq_ez(m0,n,neq,f):
+    X = 1
+    for m in range(m0, n+1):
+        X *= f(m)
+    for m in range(0,len(neq)):
+        X /= f(neq[m])
     return X
