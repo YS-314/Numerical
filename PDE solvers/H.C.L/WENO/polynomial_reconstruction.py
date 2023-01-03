f1a8d7682815f8834bf3e8f84838302bda72c273
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 10:58:27 +0800

    Added conditionals

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -3,12 +3,15 @@ import numpy
 
 def sum_neq(m0,n,neq,f):
     X = 0
+    lenneq = len(neq)
     for m in range(m0,neq[0]):
         X += f(m)
-    for M in range1(len(neq),1): 
-        for m in range1(neq[M],neq[M-1]+1):
-            X += f(m)
-    for m in range(neq[len(neq)-1]+1,n+1): #correct
+    if lenneq > 1:
+        for M in range1(lenneq,1):
+            if neq[M]-neq[M-1] > 1:
+                for m in range1(neq[M],neq[M-1]+1):
+                    X += f(m)
+    for m in range(neq[lenneq-1]+1,n+1):
         X += f(m)
     return X
 
