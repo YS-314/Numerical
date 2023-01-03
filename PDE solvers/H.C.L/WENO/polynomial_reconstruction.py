acbc5f6427b959a024f23f75e76c0b8c5a0b9ebd
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 11:18:23 +0800

    shift some functions to misc.py

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -1,27 +1,7 @@
 import math
 import numpy
 
-def sum_neq(m0,n,neq,f):
-    X = 0
-    lenneq = len(neq)
-    for m in range(m0,neq[0]):
-        X += f(m)
-    if lenneq > 1:
-        for M in range1(lenneq,1):
-            if neq[M]-neq[M-1] > 1:
-                for m in range1(neq[M],neq[M-1]+1):
-                    X += f(m)
-    for m in range(neq[lenneq-1]+1,n+1):
-        X += f(m)
-    return X
 
 def crj_u(r,s,j):
     k = r+s+1
 
-def sum_neq_ez(m0,n,neq,f): #computes unnecessary terms
-    X = 0
-    for m in range(m0, n+1):
-        X += f(m)
-    for m in range(0,len(neq)):
-        X -= f(neq[m])
-    return X
