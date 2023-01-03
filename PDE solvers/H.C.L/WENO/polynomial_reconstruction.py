3c0303b19f3cfe753cb8f3b6ea508fdec6c728d5
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 13:59:58 +0800

    reconstruction of uniform grid

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -4,9 +4,16 @@ import numpy
 #determine coefficients for WENO reconstruction scheme in a uniform grid
 def crj_u(r,s,j):
     k = r+s+1
+    X = 0
     for m in range(j+1,k+1):
+        def fcrj_n1(q):
+            return r - q + 1
+        def fcrj_n(l):
+            return prod_neq(0,k,np.sort([m,l]),fcrj_n1)
         def fcrj_d(l):
             return m - l
-        /prod_neq(0,k,[m],)
+        
+        X += sum_neq(0,k,[m],fcrj_n)/prod_neq(0,k,[m],fcrj_d) #denominator correct
+    return X
 
 
