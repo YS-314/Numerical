d355b236f3bbbaa6dbce57eb6b52ee065ea0d3b4
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 14:47:14 +0800

    call coeff as object

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -2,6 +2,16 @@ import math
 import numpy
 
 #determine coefficients for WENO reconstruction scheme in a uniform grid
+class rec_coeff:
+    def __init__(param,r,s):
+        param.r = np.array(r)
+        param.s = np.array(s)
+        param.len = min(param.r.shape,param.s.shape)
+    
+    def 
+    
+
+
 def crj_u(r,s,j):
     k = r+s+1
     X = 0
