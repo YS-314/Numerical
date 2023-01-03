b0ecd91e780b514b0a6076223d65bd69d0db7389
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-03 12:06:06 +0800

    crj uniform

--- PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
+++ PDE solvers/H.C.L/WENO/polynomial_reconstruction.py
@@ -1,7 +1,12 @@
 import math
 import numpy
 
-
+#determine coefficients for WENO reconstruction scheme in a uniform grid
 def crj_u(r,s,j):
     k = r+s+1
+    for m in range(j+1,k+1):
+        def fcrj_d(l):
+            return m - l
+        /prod_neq(0,k,[m],)
+
 
