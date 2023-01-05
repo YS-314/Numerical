6a767e54ea91cd06aa454de526246318bc79fb24
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-05 17:13:55 +0800

    add uniform grid

--- PDE solvers/grid.py
+++ PDE solvers/grid.py
@@ -1,6 +1,21 @@
 
 
+
+
 class u_xi:
+    def __init__(ui,ist, iend,delta):
+        uio.ist = ist
+        uio.iend = iend
+        uio.delta = delta
+    def ui(uio, i):
+        if 
+
+class u_xi2:
+    def __init__(ui,start,delta,i):
+        ui.start = start
+        ui.delta = delta
+        ui.ir 
+
 
 #Non uniform grid 
 class nu_xi:
