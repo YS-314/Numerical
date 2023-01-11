ca1dcbbd8704b964ccff0ed358de897139749b94
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-11 22:20:31 +0800

    poly1

--- mfun/poly_coeff.py
+++ mfun/poly_coeff.py
@@ -1,10 +1,18 @@
 import itertools
 import math
-import numpy
+import numpy as np
 from mfun.misc.misc_mfun import range1
+
+def poly1(rj, alpha = 1):
+	n = len(rj)
+	cj = [0]*(n+1)#np.zeros(n+1)
+	for j in range1(n+1):
+		
+		cj[j] = alpha*(-1)**(n+j)*math.fsum([math.prod(x)for x in list(itertools.combinations(rj,n-j))]) #list(itertools.combinations(rj,n-j))#math.fsum(
+		'''cj[j]=alpha*(-1)**(n+j)*math.fsum(itertools.combinations(rj,n-j))'''
+	return cj
+
 #coefficients for lagrange interpolating polynomial
-def poly1(alpha, rj):
-	n = length(rj)
-	cj = np.zeros(n)
-	for j in range1()
-	cj[]=(-1)**(n+j)math.fsum(itertools.combinations(rj,))
+def lpcoeff()
+
+
