8a61b85aad832299373fc5195c284d936f53e359
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-12 10:52:23 +0800

    lbpcf

--- mfun/poly_coeff.py
+++ mfun/poly_coeff.py
@@ -2,7 +2,9 @@ import itertools
 import math
 import numpy as np
 from mfun.misc.misc_mfun import range1
+from mfun.misc.neq_sp import prod_neq
 
+# for 1D
 def poly1(rj, alpha = 1):
 	n = len(rj)
 	cj = [0]*(n+1)#np.zeros(n+1)
@@ -16,12 +18,25 @@ def poly_e(cf, x):
 	xn = x
 	X = cf[0] #coefficient for x^0 (constant)
 	for j in range(1,len(cf)):
-		X += cf[j]xn
+		X += cf[j]*xn
 		xn *= x
 	return X
 
+#Lagrange polynomial
 
-#coefficients for lagrange interpolating polynomial
-def lpcoeff()
+#coefficients for lagrange interpolating polynomial (1D only)
+def lpcf():
 
 
+#coefficients of lagrange basis polynomial 
+def lbpcf(j,xi):
+	def alpw(m):
+		return xi[j] - xi[m]
+	k = len(xi)-1
+	cf = np.array()
+	alpha = 1/prod_neq(0,k,)
+	
+	
+
+#Lagrange polynomials, barycentric form 
+lp_bf()
