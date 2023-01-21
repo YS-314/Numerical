132506d88a54657de97856b4b43a1bffae1f1c9b
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-01-21 19:26:49 +0800

    Update Arrays.py

--- mfun/Arrays.py
+++ mfun/Arrays.py
@@ -1,5 +1,5 @@
 #Support for array and tensors
-#requires all lists to be numpy arrays
+#requires all lists to be numpy arrays, while multiple lists are to be stored in tuples
 import itertools
 
 #package arrays
@@ -10,6 +10,10 @@ def packarr(*arrays):
 #shape of arrays at mth dimension
 def getshp_mdim(m, arrays):
     return tuple(arr.shape[m] for arr in arrays)
+
+#
+def minshape(*arrays):
+    return tuple([min(getshp_mdim(x,arrays)) for x in range(min(getdim(arrays)))])
 
 #Get list number of dimensions in 
 def getdim(arrays):
