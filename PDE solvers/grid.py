ed8f1e000dd330ae1ef1bd7a5055ab8e94825a27
YS-314 <120360443+YS-314@users.noreply.github.com>
2023-02-07 07:46:14 +0800

    update grid.py

--- PDE solvers/grid.py
+++ PDE solvers/grid.py
@@ -1,12 +1,18 @@
 import numpy as np
 from mfun.misc.array import elwop
-
+#grid for finite difference/finite volume methods (discrete)
 #note: ist and iend must be a list
+#bt is bound type, bval is boundary value
+#bound will be a tuple of start and end values for bt == 1: .
 class u_xi:
-    def __init__(uio,ist, iend,delta, bound):
+    def __init__(uio,ist, iend,delta, bound, bval, bt = 0):
         uio.ist = np.array(ist)
         uio.iend = np.array(iend)
         uio.delta = np.array(delta)
+        uio.bt = bt
+        
+
+
         uio.bd = bound
 
     def cfi(uio,fi):
@@ -18,12 +24,18 @@ class u_xi:
             uio.fi = np.array(fi)
         else:
             raise IndexError("fi's length does not match with object")
-
+#Implement Dirichlet boundary condition for PDE
     def fi(uio,i):
-        if uio.ist<=i<=uio.iend:
-            return uio.fi[i]
-        else: 
-            return 
+        if bt = 0:
+            if uio.ist<=i<=uio.iend:
+                return uio.fi[i]
+            else: 
+                return uio.bd[]
+        elif bt == 1:
+            if uio.ist<=i<=uio.iend:
+                return uio.fi[i]
+            else: 
+                return uio.bd[]
 
     def ui(uio, i):
         return i*uio.delta
@@ -32,18 +44,18 @@ class u_xi:
         return np.array([uio.ui(I[x]) for x in np.ndindex(I.shape[:I.ndim-1])])
 
 
-class u_xi2:
+'''class u_xi2:
     def __init__(ui,start,delta,i):
         ui.start = start
         ui.delta = delta
         ui.ir 
-
+'''
 
 #Non uniform grid 
 class nu_xi:
     def __init__(grid,coord):
         grid.grid = np.array(coord, dtype = object)
-        grid.dx = np.diff(mesh.mesh)
+        grid.dx = np.diff(grid.grid)
     
     def ui(grid, i):
         return grid.grid[i]
